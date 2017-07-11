d8fc6b55e4f38cbfd0e875c35f170d7704b41f3a
Gregory Dukes <kcils360@gmail.com>
2017-07-11 15:28:52 -0700

    about-me

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-'use strict'
-alert ('Let\'s play a guessing game about me!')
+'use strict';
+alert ('Let\'s play a guessing game about me!');
 var cityBorn = prompt("Was I born in Washington?") .toLowerCase();
-  if (cityBorn == 'yes' || cityBorn== 'y') {
+  if (cityBorn == 'yes' || cityBorn == 'y') {
     alert ("Nope! I was born in Denver.");
+    console.log('City question user stated ' + cityBorn);
+  } else if (cityBorn == 'no' || cityBorn == 'n') {
+    alert ('Indeed, I was born in Denver.');
+  } else {
+    alert ('Please answer with \'yes\' or \'no\'');
   }
