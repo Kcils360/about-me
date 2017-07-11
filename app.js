43c87b1c90126b699797041d27247db31eaa9da5
Gregory Dukes <kcils360@gmail.com>
2017-07-11 15:17:17 -0700

    about-me

---
+++ app.js
@@ -0,0 +1,6 @@
+'use strict'
+alert ('Let\'s play a guessing game about me!')
+var cityBorn = prompt("Was I born in Washington?") .toLowerCase();
+  if (cityBorn == 'yes' || cityBorn== 'y') {
+    alert ("Nope! I was born in Denver.");
+  }
